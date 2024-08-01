43ab684d205fb2f2c35bc41646ccc4d5a0ff5e9c
Antonio Rezende <antoniororlandini@gmail.com>
2024-08-01 20:14:38 -0300

    first commit

---
+++ src/main/java/com/mycompany/projeto_veterinaria/Projeto_Veterinaria.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_veterinaria;
+
+import View.Tela;
+import controller.Controller;
+import model.Animal;
+import model.Cliente;
+import model.ClienteDAO;
+import model.AnimalDAO;
+import model.Consulta;
+import model.ConsultaDAO;
+import model.Especie;
+import model.EspecieDAO;
+import model.Exame;
+import model.ExameDAO;
+import model.Tratamento;
+import model.TratamentoDAO;
+import model.Veterinario;
+import model.VeterinarioDAO;
+import view.Tela02;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Projeto_Veterinaria {
+
+    public static void main(String[] args) {  
+        
+        Tela02 main = new Tela02();
+        main.show();
+        
+    }
+}
